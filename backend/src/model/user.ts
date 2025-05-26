547ce22c6bcc275e313f11f7b4547c6f1344e91e
Prathamesh-1405 <prathameshpardhiye45@gmail.com>
2025-05-26 13:08:44 +0530

    Left with, adding credentials to db

--- backend/src/model/user.ts
+++ backend/src/model/user.ts
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: {type: String, reqired: true},
   email: {type: String, required: true},
   password: {type: String},
-  
+  gId:{type: String}
 });
 
 module.exports = mongoose.model('User', userSchema);
